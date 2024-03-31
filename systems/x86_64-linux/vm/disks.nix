147a0fbb06aa250ef104ac5ff6933b528249ae45
boticelli <kandelakitina@gmail.com>
2024-03-31 15:47:40 +0300

    Fix vm/disks.nix

--- systems/x86_64-linux/vm/disks.nix
+++ systems/x86_64-linux/vm/disks.nix
@@ -1,104 +1,76 @@
 {
   disko.devices = {
-    disk = {
-      nvme0n1 = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              label = "boot";
-              name = "ESP";
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
+    disk.main = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            label = "boot";
+            name = "ESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "defaults"
+              ];
             };
+          };
+          swap = {
+            size = "4G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "root_vg";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      root_vg = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
 
-            lvm_vg = {
-              root_vg = {
-                type = "lvm_vg";
-                lvs = {
-                  root = {
-                    size = "100%FREE";
-                    content = {
-                      type = "btrfs";
-                      extraArgs = ["-f"];
-
-                      subvolumes = {
-                        "/root" = {
-                          mountpoint = "/";
-                          mountOptions = ["subvol=root" "compress=zstd" "noatime"];
-                        };
-                        "/home" = {
-                          mountpoint = "/home";
-                          mountOptions = ["subvol=home" "compress=zstd" "noatime"];
-                        };
-                        "/nix" = {
-                          mountpoint = "/nix";
-                          mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
-                        };
-                        "/persist" = {
-                          mountpoint = "/persist";
-                          mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
-                        };
-                        "/log" = {
-                          mountpoint = "/var/log";
-                          mountOptions = ["subvol=log" "compress=zstd" "noatime"];
-                        };
-                      };
-                    };
-                  };
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+                };
+                "/home" = {
+                  mountpoint = "/home";
+                  mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+                };
+                "/log" = {
+                  mountpoint = "/var/log";
+                  mountOptions = ["subvol=log" "compress=zstd" "noatime"];
                 };
               };
             };
-
-            # luks = {
-            #   size = "100%";
-            #   label = "luks";
-            #   content = {
-            #     type = "luks";
-            #     name = "cryptroot";
-            #     extraOpenArgs = [
-            #       "--allow-discards"
-            #       "--perf-no_read_workqueue"
-            #       "--perf-no_write_workqueue"
-            #     ];
-            #     content = {
-            #       type = "btrfs";
-            #       extraArgs = ["-L" "nixos" "-f"];
-            #       subvolumes = {
-            #         "/root" = {
-            #           mountpoint = "/";
-            #           mountOptions = ["subvol=root" "compress=zstd" "noatime"];
-            #         };
-            #         "/home" = {
-            #           mountpoint = "/home";
-            #           mountOptions = ["subvol=home" "compress=zstd" "noatime"];
-            #         };
-            #         "/nix" = {
-            #           mountpoint = "/nix";
-            #           mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
-            #         };
-            #         "/persist" = {
-            #           mountpoint = "/persist";
-            #           mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
-            #         };
-            #         "/log" = {
-            #           mountpoint = "/var/log";
-            #           mountOptions = ["subvol=log" "compress=zstd" "noatime"];
-            #         };
-            #       };
-            #     };
-            #   };
-            # };
           };
         };
       };
