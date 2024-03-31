64b5abf7aa3ccd941d5d7d4d79350ab91a00f75c
boticelli <kandelakitina@gmail.com>
2024-03-31 10:19:50 +0300

    First commit

---
+++ flake.nix
@@ -0,0 +1,106 @@
+{
+  description = "Boticelli's NixOS Config Flake 3";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko.url = "github:nix-community/disko";
+    hardware.url = "github:nixos/nixos-hardware";
+    sops-nix.url = "github:mic92/sops-nix";
+
+    impermanence.url = "github:nix-community/impermanence";
+    # lanzaboote.url = "github:nix-community/lanzaboote";
+
+    nixgl.url = "github:nix-community/nixGL";
+    nix-colors.url = "github:misterio77/nix-colors";
+    # nix-ld.url = "github:Mic92/nix-ld";
+    # nix-index-database.url = "github:nix-community/nix-index-database";
+    # comma.url = "github:nix-community/comma";
+
+    # hypr-contrib.url = "github:hyprwm/contrib";
+    # hypridle.url = "github:hyprwm/Hypridle";
+    # hyprlock.url = "github:hyprwm/Hyprlock";
+    # hyprpaper.url = "github:hyprwm/hyprpaper";
+
+    # hyprland-git.url = "github:hyprwm/hyprland";
+    # hyprland-xdph-git.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    # hyprland-protocols-git.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    # hyprland-nix.url = "github:spikespaz/hyprland-nix";
+    # hyprland-nix.inputs = {
+    #   hyprland.follows = "hyprland-git";
+    #   hyprland-xdph.follows = "hyprland-xdph-git";
+    #   hyprland-protocols.follows = "hyprland-protocols-git";
+    # };
+
+    # nixvim.url = "github:pta2002/nixvim";
+
+    firefox-gnome-theme = {
+      url = "github:rafaelmardojai/firefox-gnome-theme";
+      flake = false;
+    };
+
+    # copilotchat-nvim = {
+    #   url = "github:copilotc-nvim/copilotchat.nvim";
+    #   flake = false;
+    # };
+    # neorg-templates = {
+    #   url = "github:pysan3/neorg-templates";
+    #   flake = false;
+    # };
+    # arrow-nvim = {
+    #   url = "github:otavioschwanck/arrow.nvim";
+    #   flake = false;
+    # };
+
+    flake-programs-sqlite.url = "github:wamserma/flake-programs-sqlite";    
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+      snowfall = {
+        metadata = "Venus";
+        namespace = "Venus";
+        meta = {
+          name = "Venus";
+          title = "Boticelli's Nix Flake";
+        };
+      };
+
+      channels-config = {
+        allowUnfree = true;
+      };
+
+      systems.modules.nixos = with inputs; [
+        home-manager.nixosModules.home-manager
+        disko.nixosModules.disko
+        # lanzaboote.nixosModules.lanzaboote
+        impermanence.nixosModules.impermanence
+        sops-nix.nixosModules.sops
+        # nix-ld.nixosModules.nix-ld
+      ];
+
+      systems.hosts.thinkpad.modules = with inputs; [
+        hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
+      ];
+
+      # TODO: move to relevant files
+      # homes.modules = with inputs; [
+      #   impermanence.nixosModules.home-manager.impermanence
+      # ];
+
+      overlays = with inputs; [
+        nixgl.overlay
+      ];
+    };
+}
